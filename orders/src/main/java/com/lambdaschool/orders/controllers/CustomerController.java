c9d63aa67cdb50af78912560d8d9d669ca58169b
gasingdong <gasingdong@gmail.com>
2019-09-11 19:09:50 -0700

    create customer controller

---
+++ orders/src/main/java/com/lambdaschool/orders/controllers/CustomerController.java
@@ -0,0 +1,25 @@
+package com.lambdaschool.orders.controllers;
+
+import com.lambdaschool.orders.models.Customer;
+import com.lambdaschool.orders.services.CustomerService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+  @Autowired
+  private CustomerService customerService;
+
+  @GetMapping(value = "", produces = {"application/json"})
+  public ResponseEntity<?> listAllCustomers() {
+    List<Customer> results = customerService.findAll();
+    return new ResponseEntity<>(results, HttpStatus.OK);
+  }
+}
